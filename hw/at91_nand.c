cf3bee90ecb8500959250a7a0444892a233d6743
Ricky Ng-Adam <rngadam@lophilo.com>
2012-04-19 20:37:08 +0800

    nand compiles, but does not register any I/O registers

--- hw/at91_nand.c
+++ hw/at91_nand.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-#include "hw.h"
-#include "flash.h"
+#include "nand.h"
 #include "at91.h"
 
 typedef struct NandState {
-    struct NANDFlashState *nand_state;
+    NANDFlashState *nand_state;
 } NandState;
 
 //#define AT91_NAND_DEBUG
@@ -24,8 +23,9 @@ static uint64_t at91_nand_mem_read(void *opaque, target_phys_addr_t offset,
     unsigned size)
 {
     NandState *s = opaque;
+    DeviceState *d = &s->nand_state->busdev.qdev;
 
-    uint8_t res = nand_getio(s->nand_state);
+    uint8_t res = nand_getio(d);
     DPRINTF("(IP %X) read from %X (res %X)\n", g_env->regs[15], offset, res);
     return res;
 }
@@ -34,10 +34,11 @@ static void at91_nand_mem_write(void *opaque, target_phys_addr_t offset,
                 uint64_t value, unsigned size)
 {
     NandState *s = opaque;        
+    DeviceState *d = &s->nand_state->busdev.qdev;
 
-    nand_setpins(s->nand_state, offset & (1 << 22), offset & (1 << 21), 0, 1, 0);
+    nand_setpins(d, offset & (1 << 22), offset & (1 << 21), 0, 1, 0);
     DPRINTF("(IP %X) write to %X %X\n", g_env->regs[15], offset, value);
-    nand_setio(s->nand_state, value & 0xFF);
+    nand_setio(d, value & 0xFF);
 }
 
 static const MemoryRegionOps at91_nand_mmio_ops = {
@@ -49,13 +50,14 @@ static const MemoryRegionOps at91_nand_mmio_ops = {
 void at91_nand_register(NANDFlashState *st)
 {
     NandState *s;
-    int iomemtype;
+    //int iomemtype;
 
-    s = qemu_mallocz(sizeof(*s));
+    s = g_malloc(sizeof(*s));
     s->nand_state = st;
-    memory_region_init_io(&s->nand_regs_region, &at91_nand_mmio_ops, s, 
-            "at91,nand", NAND_SIZE);
+    //memory_region_init_io(&s->nand_regs_region, &at91_nand_mmio_ops, s, 
+    //        "at91,nand", NAND_SIZE);
 
+    // TODO: don't know what the equivalent registration command is here?
     //cpu_register_physical_memory(0x40000000, 0x10000000, iomemtype);
 }
 
